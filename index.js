30a3dfb77d0fee984847b6e6eb648f0fdb97202d
Kalamon9 <kmonnier@hotmail.com>
2023-01-07 20:33:42 +0000

    add favicon and font

--- index.js
+++ index.js
@@ -57,53 +57,56 @@ for (let i = 0; i < boxes.length; i++)
     value8 = boxes[7].textContent;
     value9 = boxes[8].textContent;
 
-    if (value1 != "" && value1 === value2 && value1 === value3) {
-      for (let j = 0; j <= 2; j++) {
-        boxes[j].classList.add("won");
+    const checkWin = () => {
+      if (value1 != "" && value1 === value2 && value1 === value3) {
+        for (let j = 0; j <= 2; j++) {
+          boxes[j].classList.add("won");
+        }
+        //   boxes[0].classList.add("won");
+        //   boxes[1].classList.add("won");
+        //   boxes[2].classList.add("won");
+        winner();
+      } else if (value4 != "" && value4 === value5 && value4 === value6) {
+        for (let k = 3; k <= 5; k++) {
+          boxes[k].classList.add("won");
+        }
+        winner();
+      } else if (value7 != "" && value7 === value8 && value7 === value9) {
+        for (let l = 6; l <= 8; l++) {
+          boxes[l].classList.add("won");
+        }
+        winner();
+      } else if (value1 != "" && value1 === value4 && value1 === value7) {
+        // for (let i = 0; i <= 6; i + 3) {
+        //   boxes[i].classList.add("won");
+        // }
+        boxes[0].classList.add("won");
+        boxes[3].classList.add("won");
+        boxes[6].classList.add("won");
+        winner();
+      } else if (value2 != "" && value2 === value5 && value2 === value8) {
+        boxes[1].classList.add("won");
+        boxes[4].classList.add("won");
+        boxes[7].classList.add("won");
+        winner();
+      } else if (value3 != "" && value3 === value6 && value3 === value9) {
+        boxes[2].classList.add("won");
+        boxes[5].classList.add("won");
+        boxes[8].classList.add("won");
+        winner();
+      } else if (value1 != "" && value1 === value5 && value1 === value9) {
+        boxes[0].classList.add("won");
+        boxes[4].classList.add("won");
+        boxes[8].classList.add("won");
+        winner();
+      } else if (value3 != "" && value3 === value5 && value3 === value7) {
+        boxes[2].classList.add("won");
+        boxes[4].classList.add("won");
+        boxes[6].classList.add("won");
+        winner();
       }
-      //   boxes[0].classList.add("won");
-      //   boxes[1].classList.add("won");
-      //   boxes[2].classList.add("won");
-      winner();
-    } else if (value4 != "" && value4 === value5 && value4 === value6) {
-      for (let k = 3; k <= 5; k++) {
-        boxes[k].classList.add("won");
-      }
-      winner();
-    } else if (value7 != "" && value7 === value8 && value7 === value9) {
-      for (let l = 6; l <= 8; l++) {
-        boxes[l].classList.add("won");
-      }
-      winner();
-    } else if (value1 != "" && value1 === value4 && value1 === value7) {
-      // for (let i = 0; i <= 6; i + 3) {
-      //   boxes[i].classList.add("won");
-      // }
-      boxes[0].classList.add("won");
-      boxes[3].classList.add("won");
-      boxes[6].classList.add("won");
-      winner();
-    } else if (value2 != "" && value2 === value5 && value2 === value8) {
-      boxes[1].classList.add("won");
-      boxes[4].classList.add("won");
-      boxes[7].classList.add("won");
-      winner();
-    } else if (value3 != "" && value3 === value6 && value3 === value9) {
-      boxes[2].classList.add("won");
-      boxes[5].classList.add("won");
-      boxes[8].classList.add("won");
-      winner();
-    } else if (value1 != "" && value1 === value5 && value1 === value9) {
-      boxes[0].classList.add("won");
-      boxes[4].classList.add("won");
-      boxes[8].classList.add("won");
-      winner();
-    } else if (value3 != "" && value3 === value5 && value3 === value7) {
-      boxes[2].classList.add("won");
-      boxes[4].classList.add("won");
-      boxes[6].classList.add("won");
-      winner();
-    }
+    };
+    checkWin();
   });
 
 //play again
